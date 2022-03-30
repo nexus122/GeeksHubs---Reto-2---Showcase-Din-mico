f7500ef51e956365874b3db763004a43052e6bbb
nexus122 <juan-pablo122@hotmail.es>
2022-03-30 20:46:15 +0200

    EJERCICIO SOLUCIONADO CORRIGIENDO CSS

--- script.js
+++ script.js
@@ -1,13 +1,74 @@
-function allowDrop(event) {
-    event.prevententDefault();
+let Estado = {
+    productos: [],
+    apiUrl: './assets/items.json',
+    total: 0,
 }
 
-function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
+// Llamada a la api de productos falsos
+async function getProducts(callback) {
+    let result = await fetch(Estado.apiUrl);
+    let data = await result.json();
+    Estado.productos = data;
+    callback();
 }
 
-function drop(event) {
-    event.prevententDefault();
-    var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
+// Dibujar en el DOM los productos
+function drawProducts() {
+    /* Dibujamos los elementos en el DOM */
+    Estado.productos.forEach(element => {
+        let template = `
+        <div class="card" id="${element.id}" draggable="true" ondragstart="drag(event)">            
+            <div class="card-img-top" style="background-image:url(${element.image})"></div>
+            <div class="card-body">
+                <h6 class="card-title">${element.title}</h6>
+                <p class="description">${element.description}</p>
+                <p><b>Precios:</b> ${element.price}€</p>
+            </div>
+        </div>`
+
+        document.querySelector('#shop').innerHTML += template;        
+    });
+}
+
+getProducts(drawProducts);
+
+/* Drag and drop */
+function allowDrop(ev) {
+    ev.preventDefault();    
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("html", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+
+    let data = ev.dataTransfer.getData("html");
+    let producto = Estado.productos.find(element => element.id == data);    
+
+    let cartElement = document.createElement('div');
+
+    /* Creación de un carrito de la compra */
+    cartElement.innerHTML = `
+    <span class="d-flex cart-element">        
+        <div>
+            <span class="card-title"><b>${producto.title}</b></span>
+            <span>${producto.price}€</span>
+        </div>
+        <span><button class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button></span>
+    </span>`;
+
+    Estado.total += producto.price;
+
+    document.querySelector('.total span').innerHTML = Estado.total.toFixed(2);
+    console.log(ev.target)
+    document.querySelector('.cart').appendChild(cartElement);
+
+    /* Eventos */
+    cartElement.querySelector('.btn-danger').addEventListener('click', function () {
+        Estado.total -= producto.price;
+        document.querySelector('.total span').innerHTML = Estado.total.toFixed(2);
+        this.parentElement.parentElement.remove();
+    });
 }
